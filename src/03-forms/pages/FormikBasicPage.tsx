cbb3ac36f806ca15bd093ce76fa37655b5e5d849
johannpereze <sebas.jsp@gmail.com>
2022-02-13 13:46:58 -0500

    clase 132 creamos formulario con formik

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,18 +1,45 @@
+import { useFormik } from 'formik';
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
       <h1>Formik basic Tutorial</h1>
-      <form noValidate>
+      <form onSubmit={handleSubmit} noValidate>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" />
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={values.firstName}
+        />
         <span>First name is required</span>
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={values.lastName}
+        />
         <span>Last name is required</span>
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={values.email}
+        />
         <button type="submit">Submit</button>
         <span>E-mail is required</span>
         <span>Check for a valid email format</span>
